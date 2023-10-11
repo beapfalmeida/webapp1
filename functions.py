06d267a4c554639f749424174da7d01e906cf610
beatriz <beatrizoffline@gmail.com>
2023-10-11 12:23:44 +0100

    try to fix error UnicodeDecodeError:

--- functions.py
+++ functions.py
@@ -1,13 +1,13 @@
 def get_todos(filepath='todos.txt'):
     """ Read text file and return the list of todo items """
-    with open(filepath, 'r', encoding='UTF-8') as file:
+    with open(filepath, 'r', encoding='iso-8859-1') as file:
         todos_local = file.readlines()
     return todos_local
 
 
 def write_todos(todos_arg, filepath='todos.txt'):       # (onde escrevo, o que escrevo)
     """ Write in the text file the new todo items """
-    with open(filepath, 'w', encoding='UTF-8') as file:
+    with open(filepath, 'w', encoding='iso-8859-1') as file:
         file.writelines(todos_arg)
 
 
